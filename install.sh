6bdd82abcdb58ba929a3519a7b451140bf1db2be
ricktonycr <ricktonycr@gmail.com>
2022-12-03 12:20:23 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ tar -xzvf hadoop-3.3.4.tar.gz &&
 mv hadoop-3.3.4 hadoop &&
 echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/" >> ~/.bashrc &&
 echo "export HADOOP_HOME=/home/ubuntu/hadoop" >> ~/.bashrc &&
-echo "export HADOOP_CONF=$HADOOP_HOME/conf"  >> ~/.bashrc &&
-echo "export PATH=$PATH:$JAVA_HOME:$HADOOP_HOME/bin" >> ~/.bashrc
+echo "export HADOOP_CONF=\$HADOOP_HOME/conf"  >> ~/.bashrc &&
+echo "export PATH=\$PATH:\$JAVA_HOME:\$HADOOP_HOME/bin" >> ~/.bashrc &&
 source ~/.bashrc

dffbb4c0e9552b7e0bc9e1fb6cb40d662085a7c8
ricktonycr <ricktonycr@gmail.com>
2022-12-03 12:06:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt update &&
 sudo apt install default-jdk &&
 wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz &&
 tar -xzvf hadoop-3.3.4.tar.gz &&
-sudo mv hadoop-3.3.4 hadoop &&
+mv hadoop-3.3.4 hadoop &&
 echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/" >> ~/.bashrc &&
 echo "export HADOOP_HOME=/home/ubuntu/hadoop" >> ~/.bashrc &&
 echo "export HADOOP_CONF=$HADOOP_HOME/conf"  >> ~/.bashrc &&
